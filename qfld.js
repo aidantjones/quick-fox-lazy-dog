4ddaa43b27558c6c03711fc0767d14ea73b2bc30
Aidan Jones <1606733+aidantjones@users.noreply.github.com>
2020-02-07 14:32:17 -0800

    Added program logic and preliminary Javascript program

---
+++ qfld.js
@@ -0,0 +1,22 @@
+function line_amount_generator(min, max) {
+  Math.floor(Math.random() * (10*(max - min + 1))) + min;
+}
+
+var line_amount = line_amount_generator(4,25);
+
+var stanza_randomizer = line_amount_generator(1,11);
+
+var stanza_total += stanza_randomizer;
+
+if stanza_total > line_amount {
+ var last_return = str.lastIndexOf("\r");
+ var last_return = null;
+ function stringRandomizer()
+} else {
+
+}
+
+
+
+console.log(final_poem)
+//document.getElementById("poem").innerHTML = final-poem;
